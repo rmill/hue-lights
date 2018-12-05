d2ff827e128ee1a8b43b71c2c187599289dba68d
Ryan Miller <rmill865@gmail.com>
2018-12-05 08:53:51 -0700

    Added Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  // { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
